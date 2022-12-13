2b01f777129d0f1568c7c7e4a01aa75635988a76
Superbuns14 <55467221+Superbuns14@users.noreply.github.com>
2022-12-13 15:48:43 -0500

    Create alpha_beta.py

---
+++ alpha_beta.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+import random
+import math
+
+def mcts_policy(time_limit, h):
+    def fxn(pos):
+        #move = MCTS(pos, time_limit) # if MCTS was class
+        #res =  move.till_end_time() # if run was function in class
+        val, move = alpha_beta(pos, 5, -math.inf, math.inf, h)
+        return move
+    return fxn
+
+
+
+def alpha_beta(pos, depth, alpha, beta, h):
+    if pos.is_terminal() or depth == 0:
+        return (h.evaluate(pos), None)
+    else:
+        if pos.actor() == 0:
+            # max player
+            best_value = -h.inf
+            best_move = None
+            moves = pos.get_actions()
+            for move in moves:
+                child = pos.successor(move)
+                mm, _ = alpha_beta(child, depth - 1, alpha, beta, h)
+                if mm > best_value:
+                    best_value = mm
+                    best_move = move
+                alpha = max(best_value, alpha)
+                if alpha > beta:
+                    break
+            return (best_value, best_move)
+        else:
+            # min player
+            best_value = h.inf
+            best_move = None
+            moves = pos.get_actions()
+            for move in moves:
+                child = pos.successor(move)
+                mm, _ = alpha_beta(child, depth - 1, alpha, beta, h)
+                if mm < best_value:
+                    best_value = mm
+                    best_move = move
+                beta = min(beta, best_value)
+                if alpha > beta:
+                    break
+            return (best_value, best_move)
