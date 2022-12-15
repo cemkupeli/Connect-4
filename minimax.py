24f995deb9a074656cc0301d0db7cd294f20d88f
Superbuns14 <55467221+Superbuns14@users.noreply.github.com>
2022-12-15 14:06:36 -0500

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -2,14 +2,21 @@
 
 import math
 
-def minimax_policy(depth, h):
+def minimax_policy(depth, randomness):
     def fxn(pos):
-        value, move = minimax(pos, depth, h)
+        if randomness > 1:
+            raise ValueError('randomness can not be more than 1')
+        num = random.random()
+        if num < randomness:
+            moves = pos.get_actions()
+            move = random.choice(moves)
+            return move
+        value, move = minimax(pos, depth)
         return move
     return fxn
 
 
-def minimax(pos, depth, h):
+def minimax(pos, depth):
     ''' Returns the minimax value of the given position, with the given heuristic function
         applied at the given depth.
 
@@ -17,12 +24,7 @@ def minimax(pos, depth, h):
         depth -- a nonnegative integer
         h -- a heuristic function that can be applied to pos and all its successors
     '''
-    # print("Minimax called with depth", depth, "on following position:")
-    # print(pos)
-    # print("Position is terminal:", pos.is_terminal())
-    # print("Current actor is:", pos.actor())
     if pos.is_terminal() or depth == 0:
-        # print("Position is terminal or depth is 0, returning heuristic value")
         return (pos.heuristic(), None)
     else:
         if pos.actor() == 0:
@@ -30,37 +32,22 @@ def minimax(pos, depth, h):
             best_value = -math.inf
             best_move = None
             moves = pos.get_actions()
-            # if moves:
-            #     print("Available moves:", moves)
-            # else:
-            #     print("No moves available")
             for move in moves:
-                # print("Evaluating move", move, "for the following position:")
-                # print(pos)
                 child = pos.successor(move)
-                # print("Successor is the following position:")
-                # print(child)
-                mm, _ = minimax(child, depth - 1, h)
+                mm, _ = minimax(child, depth - 1)
+                moves = pos.get_actions()
                 if mm > best_value:
                     best_value = mm
-                    best_move = move
+                    best_move = move  
             return (best_value, best_move)
         else:
             # min player
             best_value = math.inf
             best_move = None
             moves = pos.get_actions()
-            # if moves:
-            #     print("Available moves:", moves)
-            # else:
-            #     print("No moves available")
             for move in moves:
-                # print("Evaluating move", move, "for the following position:")
-                # print(pos)
                 child = pos.successor(move)
-                # print("Successor is the following position:")
-                # print(child)
-                mm, _ = minimax(child, depth - 1, h)
+                mm, _ = minimax(child, depth - 1)
                 if mm < best_value:
                     best_value = mm
                     best_move = move
