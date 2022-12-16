b375d2383123c3566457fb9304dd6df5077bd55d
Superbuns14 <55467221+Superbuns14@users.noreply.github.com>
2022-12-15 20:20:12 -0500

    Add comments

--- alpha_beta.py
+++ alpha_beta.py
@@ -6,6 +6,7 @@ import math
 
 def alpha_beta_policy(depth, randomness):
     def fxn(pos):
+        # element of randomness, return random move randomness percent of time
         if randomness > 1:
             raise ValueError('randomness can not be more than 1')
         num = random.random()
@@ -13,28 +14,33 @@ def alpha_beta_policy(depth, randomness):
             moves = pos.get_actions()
             move = random.choice(moves)
             return move
+
+        # find best move according to minimax otherwise
         val, move = alpha_beta(pos, depth, -math.inf, math.inf)
         return move
     return fxn
 
 def alpha_beta(pos, depth, alpha, beta):
+    # if position is terminal or depth goal is reached, use heuristic function
     if pos.is_terminal() or depth == 0:
         return (pos.heuristic(), None)
     else:
-        
-
         if pos.actor() == 0:
             # max player
             best_value = -math.inf
             best_move = None
             moves = pos.get_actions()
+            # iterate through all moves
             for move in moves:
                 child = pos.successor(move)
                 mm, _ = alpha_beta(child, depth - 1, alpha, beta)
+                # update node to best move and update value
                 if mm > best_value:
                     best_value = mm
                     best_move = move
+                # update alpha to highest value
                 alpha = max(best_value, alpha)
+                # if alpha is more than beta, prune it
                 if alpha > beta:
                     break
             return (best_value, best_move)
@@ -49,7 +55,9 @@ def alpha_beta(pos, depth, alpha, beta):
                 if mm < best_value:
                     best_value = mm
                     best_move = move
+                # update beta to min value between the two
                 beta = min(beta, best_value)
+                # if alpha is greater than beta, prune it
                 if alpha > beta:
                     break
             return (best_value, best_move)
